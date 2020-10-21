02749187607cba3e1d41bcbfbed8601b491f9cb2
Ivan Dramaliev <idrm@users.noreply.github.com>
2020-10-21 14:46:43 +0300

    initial commit

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ namespace Html2Pdf
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
